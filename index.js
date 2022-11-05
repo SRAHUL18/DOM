d1b2f1f3923a48f5fa3c515d2d42a7148ca593ec
SRAHUL18 <103924449+SRAHUL18@users.noreply.github.com>
2022-11-05 23:02:56 +0530

    Add dom initial files

---
+++ index.js
@@ -0,0 +1,7 @@
+document.query
+var heading=document.firstElementChild.lastElementChild.firstElementChild;
+// heading.style.color="blue";
+
+document.getElementsByTagName("li")[2].innerHTML="rahul";
+document.getElementsByClassName("btn")[0].style.color="red"; 
+document.querySelector(".btn").style.backgroundColor="yellow";
